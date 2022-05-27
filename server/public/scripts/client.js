1677e122585b3a9e596e3ccd2e95b6741d1463ae
Alexander Ratanas <alexanderratanas@gmail.com>
2022-05-26 22:07:07 -0500

    todo function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,22 @@ console.log("client connected");
 $(document).ready(function () {
   console.log("ready!");
 });
+
+//function to get all todos
+function getTodos() {
+  //empty DOM
+  $("#taskList").empty();
+  $.ajax({
+    type: "GET",
+    url: "/todo",
+  })
+    .then((response) => {
+      console.log(response);
+      //   display(response)
+    })
+    .catch((err) => {
+      console.log("fuck...", err);
+    });
+}
+//function to display all todos and delete button
+//function to check off
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -2,4 +2,8 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../modules/pool");
 
+//get router
+router.get('/', (req, res)=>)
+//post router
+
 module.exports = router;
