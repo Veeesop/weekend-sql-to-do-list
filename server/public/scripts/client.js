af909a66f2a4cb03d3412dcae6206e3ce3285f10
Alexander Ratanas <alexanderratanas@gmail.com>
2022-05-26 23:05:41 -0500

    display function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,6 +5,19 @@ $(document).ready(function () {
   getTodos();
 });
 
+//function to display all todos and delete button
+function display(response) {
+  console.log(response);
+  for (let i = 0; i < response.length; i++) {
+    let todo = response[i];
+    $("#taskList").append(`
+    <div class="todoDiv" data-id=${todo.id}>
+        <li>${todo.task}</li><button class="deleteBtn">Delete</button>
+    </div>
+      `);
+  }
+}
+
 //function to get all todos
 function getTodos() {
   //empty DOM
@@ -15,11 +28,15 @@ function getTodos() {
   })
     .then((response) => {
       console.log(response);
-      //   display(response)
+      display(response);
     })
     .catch((err) => {
       console.log("fuck...", err);
     });
 }
-//function to display all todos and delete button
+//function to delete
+function deleteTodo() {
+  let id = $(this).parents("div").data("id");
+}
+
 //function to check off
